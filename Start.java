c866b464eef7567fdd965e2fdd726bbbb6d9bde8
lurch <bobbaumann.home@gmail.com>
2024-02-15 20:12:43 +0100

    init

---
+++ Start.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Start {
+
+    public String tempAll;
+
+    public static void main(String[] args) {
+        System.out.println("OI");
+
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
